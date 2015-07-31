db6e3f7f5e920984221139ea5457ac5a94e313f7
E Kennedy <ekennedy@cern.ch>
2015-07-31 13:11:32 +0200

    debug statements

--- MyRootMaker/src/RootMaker.cc
+++ MyRootMaker/src/RootMaker.cc
@@ -1341,49 +1341,55 @@ void RootMaker::analyze(const edm::Event &iEvent, const edm::EventSetup &iSetup)
         iEvent.getByToken(triggerPrescales_, triggerPrescales);
 
         const edm::TriggerNames &names = iEvent.triggerNames(*triggerBits);
-        //std::cout << "\n === TRIGGER PATHS === " << std::endl;
-        //for(unsigned int i = 0, n = triggerBits->size(); i < n; ++i) {
-        //    std::cout << "Trigger " << names.triggerName(i) <<
-        //              ", prescale " << triggerPrescales->getPrescaleForIndex(i) <<
-        //              ": " << (triggerBits->accept(i) ? "PASS" : "fail (or not run)")
-        //              << std::endl;
-        //}
+        if (cdebug) {
+            std::cout << "\n === TRIGGER PATHS === " << std::endl;
+            for(unsigned int i = 0, n = triggerBits->size(); i < n; ++i) {
+                std::cout << "Trigger " << names.triggerName(i) <<
+                          ", prescale " << triggerPrescales->getPrescaleForIndex(i) <<
+                          ": " << (triggerBits->accept(i) ? "PASS" : "fail (or not run)")
+                          << std::endl;
+            }
+        }
 
         // Trigger Objects
         for(pat::TriggerObjectStandAlone obj : *triggerObjects) {  // note: not "const &" since we want to call unpackPathNames
             obj.unpackPathNames(names);
-//        std::cout << "\tTrigger object:  pt " << obj.pt() << ", eta " << obj.eta() << ", phi " << obj.phi() << std::endl;
-            // Print trigger object collection and type
-            //
-//       std::cout << "\t   Collection: " << obj.collection() << std::endl;
-//        std::cout << "\t   Type IDs:   ";
-//        for (unsigned h = 0; h < obj.filterIds().size(); ++h) {
-//            std::cout << " " << obj.filterIds()[h];
-//        }
+            if (cdebug) {
+                std::cout << "\tTrigger object:  pt " << obj.pt() << ", eta " << obj.eta() << ", phi " << obj.phi() << std::endl;
+                // Print trigger object collection and type
+                std::cout << "\t   Collection: " << obj.collection() << std::endl;
+                std::cout << "\t   Type IDs:   ";
+                for (unsigned h = 0; h < obj.filterIds().size(); ++h) {
+                    std::cout << " " << obj.filterIds()[h];
+                }
 
-            // Print associated trigger filters
-//        std::cout << "\t   Filters:    ";
-//        for (unsigned h = 0; h < obj.filterLabels().size(); ++h) std::cout << " " << obj.filterLabels()[h];
-//        std::cout << std::endl;
+               // Print associated trigger filters
+               std::cout << "\t   Filters:    ";
+               for (unsigned h = 0; h < obj.filterLabels().size(); ++h) {
+                   std::cout << " " << obj.filterLabels()[h];
+               }
+               std::cout << std::endl;
+            }
             std::vector<std::string> pathNamesAll  = obj.pathNames(false);
             std::vector<std::string> pathNamesLast = obj.pathNames(true);
             // Print all trigger paths, for each one record also if the object is associated to a 'l3' filter (always true for the
             // definition used in the PAT trigger producer) and if it's associated to the last filter of a successfull path (which
             // means that this object did cause this trigger to succeed; however, it doesn't work on some multi-object triggers)
-
-//        std::cout << "\t   Paths (" << pathNamesAll.size()<<"/"<<pathNamesLast.size()<<"):    ";
-//        for (unsigned h = 0, n = pathNamesAll.size(); h < n; ++h) {
-//            bool isBoth = obj.hasPathName( pathNamesAll[h], true, true );
-//            bool isL3   = obj.hasPathName( pathNamesAll[h], false, true );
-//            bool isLF   = obj.hasPathName( pathNamesAll[h], true, false );
-//            bool isNone = obj.hasPathName( pathNamesAll[h], false, false );
-//            std::cout << "   " << pathNamesAll[h];
-//            if (isBoth) std::cout << "(L,3)";
-//            if (isL3 && !isBoth) std::cout << "(*,3)";
-//            if (isLF && !isBoth) std::cout << "(L,*)";
-//            if (isNone && !isBoth && !isL3 && !isLF) std::cout << "(*,*)";
-//        }
-//        std::cout << std::endl;
+            if (cdebug) {
+                std::cout << "\t   Paths (" << pathNamesAll.size()<<"/"<<pathNamesLast.size()<<"):    ";
+                for (unsigned h = 0, n = pathNamesAll.size(); h < n; ++h) {
+                    bool isBoth = obj.hasPathName( pathNamesAll[h], true, true );
+                    bool isL3   = obj.hasPathName( pathNamesAll[h], false, true );
+                    bool isLF   = obj.hasPathName( pathNamesAll[h], true, false );
+                    bool isNone = obj.hasPathName( pathNamesAll[h], false, false );
+                    std::cout << "   " << pathNamesAll[h];
+                    if (isBoth) std::cout << "(L,3)";
+                    if (isL3 && !isBoth) std::cout << "(*,3)";
+                    if (isLF && !isBoth) std::cout << "(L,*)";
+                    if (isNone && !isBoth && !isL3 && !isLF) std::cout << "(*,*)";
+                }
+                std::cout << std::endl;
+            }
         }
 
         ///////////////////////////////////////////////////////////////////////////////////////////////////
